170b17389fa74bb2fd6539932fe409bc870e2815
Ramzy Kermad <kermad.r@sncf.fr>
2024-08-11 13:12:03 +0200

    Updated code for lesson 10

--- L10_kotlin_data_class.kt
+++ L10_kotlin_data_class.kt
@@ -1,3 +1,16 @@
 fun main () {
-	
+	val person = Person("Alice", 30)
+	println(person.toString()) 	
+
+	// equals()
+	val person1 = Person("Alice", 30)
+	val person2 = Person("Alice", 30)
+	println(person1 == person2) // Affiche "true"
+
+	val person3 = person.copy(age = 31)
+	println(person3) // Affiche "Person(name=Alice, age=31)"
+
 }
+
+// Déclaration d'une data class
+data class Person(val name: String, val age: Int)
