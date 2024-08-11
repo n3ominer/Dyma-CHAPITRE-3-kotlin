a82230e34afd13b07ec679cd58643cec5f5e81a8
Ramzy Kermad <kermad.r@sncf.fr>
2024-08-11 13:09:44 +0200

    Updated code for lesson 11

--- L11_kotlin_interfaces.kt
+++ L11_kotlin_interfaces.kt
@@ -1,3 +1,70 @@
-fun main() {
-	
+private fun mainFunc() {
+    val dog = Dog()
+    dog.makeSound()
+    dog.eat()
+
+
+    val dolphin = Dolphin()
+    dolphin.eat()
+    dolphin.makeSound()
+    dolphin.swim()
+
+    val vehicles: List<Movable> = listOf(Car(), Plane())
+
+    for (vehicle in vehicles) {
+        vehicle.move()
+    }
+}
+
+interface Animal {
+
+    fun makeSound(): String
+
+    fun eat() {
+        Log.d("Animal interface", "Eating")
+    }
+
+}
+
+
+interface Named {
+    val name: String
+    val lastName: String
+}
+
+class Dog : Animal {
+    override fun makeSound(): String {
+        return "Waf-Waf"
+    }
+
+}
+
+
+class Dolphin: Animal, Swimmer {
+    override fun makeSound(): String {
+        return "Clik-clik"
+    }
+}
+
+
+
+interface Movable {
+
+    fun move(): String
+}
+
+class Car: Movable {
+    override fun move(): String {
+        Log.d("Movable", "La voiture roule sur la route")
+        return "LA voiture roule"
+    }
+}
+
+class Plane: Movable {
+    override fun move(): String {
+        Log.d("Movable", "L'avion vole dans les airs")
+        return "L'avion vole"
+    }
+
+
 }
